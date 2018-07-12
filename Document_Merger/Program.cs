575f7bf87996fec33ec1052cfbf1f4e0b57ddee0
parkerhansen <pqh8t2@mail.missouri.edu>
2018-07-12 14:49:45 -0700

    Basic outline created

--- Document_Merger/Program.cs
+++ Document_Merger/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
 
 namespace Document_Merger
 {
     class Program
     {
+        static string txtFile = "{0}{1}.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Doucment Merger");
+
+            do{
+                Console.Write("Name of First Text File: ");
+                string firstFile = Check();
+                Console.Write("Name of Second Text File: ");
+                string secondFile = Check();
+                Console.WriteLine(CreateName(firstFile, secondFile));
+                Console.Write("Would you like to merge two more files? (y/n): ");
+            }
+            while (Console.ReadLine().ToLower().Equals("y"));
         }
+
+        static string Check()
+        {
+            bool valid = false;
+            string input = null;
+            while (!valid)
+            {
+                input = Console.ReadLine();
+                valid = true;
+                if (File.Exists(input) == false)
+                {
+                    Console.WriteLine("File does not exist, enter file name again: ");
+                    valid = false;
+                }
+            }
+            return input;
+        }
+
     }
 }
